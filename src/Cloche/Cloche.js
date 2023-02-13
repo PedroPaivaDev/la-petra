d3b4b4c98443d03e61576653b1394177179c13be
Pedro <pedropaivadev@gmail.com>
2023-02-13 20:21:23 -0300

    cloche component

---
+++ src/Cloche/Cloche.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Cloche.module.css';
+import { useNavigate } from 'react-router-dom';
+
+import Layer from '../components/Layer/Layer';
+
+import Logotipo from '../assets/Logotipo.png';
+import strawberry from '../assets/strawberry.jpg';
+// import chocolate from '../assets/chocolate-balls.png';
+// import sprinkles from '../assets/sprinkles.png';
+// import cream from '../assets/cream.jpg';
+
+const Cloche = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cloche}></div>
+      <div className={styles.layers}>
+        <div className={styles.logo} onClick={() => navigate("sobre")}>
+          <div className={styles.shadow}></div>
+          <img src={Logotipo} height="150px" alt="Logotipo"/>
+        </div>
+        <Layer bg={strawberry} position={0}>produtos</Layer>
+      </div>
+    </div>
+  )
+}
+
+export default Cloche;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,33 +1,12 @@
 import React from 'react';
+import Cloche from '../../Cloche/Cloche';
 import styles from './Home.module.css';
-import { useNavigate } from 'react-router-dom';
-
-import Layer from '../../components/Layer/Layer';
-
-import Logotipo from '../../assets/Logotipo.png';
-import strawberry from '../../assets/strawberry.jpg';
-import chocolate from '../../assets/chocolate-balls.png';
-import sprinkles from '../../assets/sprinkles.png';
-import cream from '../../assets/cream.jpg';
 
 const Home = () => {
-
-  const navigate = useNavigate();
-
   return (
-    <>
-      <div className={styles.cup}></div>
-      <div className={styles.layers}>
-        <div className={styles.logo} onClick={() => navigate("sobre")}>
-          <div className={styles.shadow}></div>
-          <img src={Logotipo} height="150px" alt="Logotipo"/>
-        </div>
-        <Layer bg={strawberry} position={210}>produtos</Layer>
-        <Layer bg={sprinkles} position={140}>encomendas</Layer>
-        <Layer bg={chocolate} position={70}>contato</Layer>
-        <Layer bg={cream} position={0}>local</Layer>
-      </div>
-    </>
+    <div className={styles.container}>
+      <Cloche/>
+    </div>
   )
 }
 
