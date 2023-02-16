0a2fafca193b7f19835a1b01450ebd3dc753ded5
Pedro <pedropaivadev@gmail.com>
2023-02-16 10:56:14 -0300

    contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import styles from './Contact.module.css';
+
+import goomer from '../../assets/iconGoomer.png';
+import ifood from '../../assets/iconIfood.png';
+import whatsapp from '../../assets/iconWhatsapp.png';
 
 const Contact = () => {
   return (
-    <div className='title'>Contatos e Horários</div>
+    <div className={styles.container}>
+      <h2>Horários de Funcionamento</h2>
+      <p>Terça a Sábado: 12:30h às 18:00h</p>
+      <p>Domingo: 11:30h às 14:30h</p>
+      <p style={{color: 'tomato'}}>Não abre na segunda-feira</p>
+      
+      <h2>Contato</h2>
+      <a href='https://l-petra.goomer.app/'>
+        <img src={goomer} alt="goomer"/>
+        Peça mais barato aqui
+      </a>
+      <a href='https://www.ifood.com.br/delivery/bom-despacho-mg/la-petra-novo-sao-jose/ffb8ee69-9fd7-46bb-81d1-7a3a290d80db?utm_medium=share'>
+        <img src={ifood} alt="goomer"/>
+        Peça pelo Ifood
+      </a>
+      <a href='https://api.whatsapp.com/send/?phone=5537988031002&text&type=phone_number&app_absent=0'>
+        <img src={whatsapp} alt="goomer"/>
+        Encomende seu bolo personalizado
+      </a>
+
+      <h2>Localização</h2>
+      <p>Av. Doutor Roberto de Melo Queiroz</p>
+      <p>Número ..., Bairro Novo São José</p>
+      <p>Bom Despacho / MG</p>
+    </div>
   )
 }
 
