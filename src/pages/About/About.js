cdda36e7c7c8720fa26ddaf27c35472d9f342d5e
Pedro <pedropaivadev@gmail.com>
2023-02-20 19:15:23 -0300

    new loading svg

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,9 +3,9 @@ import styles from './About.module.css';
 
 const About = () => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} animeLeft`}>
       <h2>Sobre a La Petra</h2>
-      <div className={styles.element}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique saepe ea aut eum aliquid perferendis repellendus maxime neque nesciunt fuga consequuntur, reprehenderit, nostrum illum suscipit corrupti minus voluptas assumenda mollitia? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, quos, pariatur libero vitae quae id, ut accusamus dicta nulla voluptate exercitationem beatae sunt similique repudiandae labore quia officia deserunt odio!</div>  
+      <div className={styles.element}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique saepe ea aut eum aliquid perferendis repellendus maxime neque nesciunt fuga consequuntur, reprehenderit, nostrum illum suscipit corrupti minus voluptas assumenda mollitia? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, quos, pariatur libero vitae quae id, ut accusamus dicta..</div>
     </div>
   )
 }
