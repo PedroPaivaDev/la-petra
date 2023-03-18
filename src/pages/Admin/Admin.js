98937b85aec5fef8e39c43e70abcfa20815347a0
Pedro <pedropaivadev@gmail.com>
2023-03-18 19:52:20 -0300

    firebase method onValue for getProducts function

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,32 +1,61 @@
 import React from 'react';
-import { registerProductOrder, createNewProduct } from '../../services/firebase';
+import styles from './Admin.module.css';
+import { registerProductsOrder, createNewProduct, getProducts } from '../../services/firebase';
 
 import Button from '../../components/Forms/Button';
+import useForm from '../../hooks/useForm';
+import Input from '../../components/Forms/Input';
 
 const Admin = () => {
+  const [products, setProducts] = React.useState();
+  const [submitError, setSubmitError] = React.useState();
+  const id = useForm();
+  const name = useForm();
+  const description = useForm();
+  const price = useForm();
+  const image = useForm("https://raw.githubusercontent.com/PedroPaivaDev/la-petra/v0.2/src/assets/easter/ovoDeliciaCrocante-1.jpg");
+
+  function handleSubmit() {    
+    if(id.value.length<=0 || name.value.length<=0 || description.value.length<=0 || price.value.length<=0 || image.value.length<=0) {
+      setSubmitError(true);
+      return true;
+    } else {
+      setSubmitError(false);
+      return false;
+    }
+  }
   
   function create() {
-    const id = 0;
-    const name = 'Fatia coco';
-    const description = 'Fatia coco com doce de leite';
-    const price = 13;
-    const image = "https://raw.githubusercontent.com/PedroPaivaDev/la-petra/v0.2/src/assets/easter/ovoDeliciaCrocante-1.jpg";
-    createNewProduct(id, name, description, price, image);
+    if(handleSubmit()) return;
+    createNewProduct(id.value, name.value, description.value, price.value, image.value);
   }
 
   function order() {
-    const id = 0;
-    const name = 'Fatia coco';
-    const description = 'Fatia coco com doce de leite';
-    const price = 50;
-    const image = "https://raw.githubusercontent.com/PedroPaivaDev/la-petra/v0.2/src/assets/easter/ovoDeliciaCrocante-1.jpg";
-    registerProductOrder(id, name, description, price, image);
+    if(handleSubmit()) return;
+    registerProductsOrder(id.value, name.value, description.value, price.value, image.value);
   }
 
+  function showProducts() {
+    getProducts(setProducts);
+  }
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setSubmitError(false);
+    },4000)
+  },[submitError])
+
   return (
-    <div>
-      <Button onClick={create}>Adicionar produto</Button>
-      <Button onClick={order}>Registrar Pedido</Button>
+    <div className={styles.container}>
+      <h2>Cadastro de Produtos</h2>
+      <Input label="Id:" type="text" name="id" {...id}/>
+      <Input label="Nome do Produto:" type="text" name="name" {...name}/>
+      <Input label="Descrição:" type="text" name="description" {...description}/>
+      <Input label="Preço:" type="text" name="price" {...price}/>
+      <Input label="Imagem:" type="text" name="image" {...image}/>
+      <Button onClick={create} submitError={submitError}>Adicionar produto</Button>
+      <Button onClick={order} submitError={submitError}>Registrar Pedido</Button>
+      <Button onClick={showProducts} submitError={submitError}>Ver Produtos</Button>
     </div>
   )
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Import Admin SDK
-import { getDatabase, ref, set, child, push } from "firebase/database";
+import { getDatabase, ref, set, child, push, onValue } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -41,8 +41,8 @@ export function createNewProduct(id, name, description, price, image) {
 }
 
 //Para registrar pedidos de clientes
-export function registerProductOrder(id, name, description, price, image) {
-  const productsRef = ref(db, 'products');
+export function registerProductsOrder(id, name, description, price, image) {
+  const ordersRef = ref(db, 'productsOrders');
   const product = {
     id: id,
     name: name,
@@ -50,5 +50,17 @@ export function registerProductOrder(id, name, description, price, image) {
     price: price,
     image: image
   };
-  push(productsRef, product)
+  push(ordersRef, product)
+}
+
+export function getProducts(setState) {
+  const productsRef = ref(db, 'products');
+  let products = [];
+  onValue(productsRef, (snapshot) => {
+    snapshot.forEach(product => {
+      products = [...products, product.val()]
+    })
+    setState(products)
+    console.log(products)
+  }, {onlyOnce: true})
 }
